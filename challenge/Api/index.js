700662ffea36562b8c6ed8cef4260b48b9a25bda
cristianocsl <cslcristiano@gmail.com>
2022-07-25 16:07:39 -0300

    refact: instancia AxiosInstance passando a url base

--- challenge/Api/index.js
+++ challenge/Api/index.js
@@ -3,11 +3,13 @@ const App = require('./App');
 const ReposController = require('./controllers/ReposController');
 const ReposRouter = require('./controllers/ReposRouter');
 const AxiosInstance = require('./helpers/AxiosInstance');
+const { url } = require('./helpers/url');
 const ReposModel = require('./models/ReposModel');
 const ReposService = require('./services/ReposService');
 
 const app = new App();
-const axiosInstance = new AxiosInstance();
+
+const axiosInstance = new AxiosInstance(url);
 const reposModel = new ReposModel(axiosInstance);
 const reposService = new ReposService(reposModel);
 const reposController = new ReposController(reposService);
