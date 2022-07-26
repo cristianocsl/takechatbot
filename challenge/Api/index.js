dace47f32e726fecab7b49007cc939ed08bb4f59
cristianocsl <cslcristiano@gmail.com>
2022-07-26 19:45:47 -0300

    refact: exporta api para utilizá-la nos testes

--- challenge/Api/index.js
+++ challenge/Api/index.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
+const url = require('./helpers/url');
 const App = require('./App');
 const ReposController = require('./controllers/ReposController');
 const ReposRouter = require('./controllers/ReposRouter');
 const AxiosInstance = require('./helpers/AxiosInstance');
-const { url } = require('./helpers/url');
 const ReposModel = require('./models/ReposModel');
 const ReposService = require('./services/ReposService');
 
 const app = new App();
-
 const axiosInstance = new AxiosInstance(url);
 const reposModel = new ReposModel(axiosInstance);
 const reposService = new ReposService(reposModel);
@@ -18,4 +17,7 @@ const reposRouter = new ReposRouter(reposController);
 app.addRouter(reposRouter.getRouter());
 
 const { PORT } = process.env;
+
+module.exports = app;
+
 app.start(PORT || 3000);
